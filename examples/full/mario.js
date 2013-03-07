818c92caae90412e526991cacf168da06d648cbb
Raynos <raynos2@gmail.com>
2013-03-06 21:53:30 -0800

    updated examples

---
+++ examples/full/mario.js
@@ -0,0 +1,8 @@
+/* Application Model */
+var Mario = {
+    xPosition: 0
+    , yPosition: 0
+    , xVelocity: 0
+    , yVelocity: 0
+    , direction: "right"
+}
--- examples/react/mouse-position-image.js
+++ examples/react/mouse-position-image.js
@@ -3,15 +3,15 @@ var map = require("../../signal/map")
 var MousePosition = require("../../input").MousePosition
 var render = require("../../render")
 
-function resizableYogi(edgeLength) {
-    return image(edgeLength, edgeLength, "http://elm-lang.org/yogi.jpg")
-}
-
 var edgeLength = map(MousePosition(), function (pos) {
     return Math.max(100, Math.max(pos.x, pos.y))
 })
+edgeLength // application state
+// =>
 
-var main = map(edgeLength, resizableYogi)
+var main = map(edgeLength, function resizableYogi(edgeLength) {
+    return image(edgeLength, edgeLength, "http://elm-lang.org/yogi.jpg")
+}) // application view
 
 render(main, false)
 // =>
